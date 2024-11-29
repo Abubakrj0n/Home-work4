0fc61cc28b8ae0ae2d7e7d2e3eaaeb7a9093c4f4
Abubakrj0n <big500530@gmail.com>
2024-11-29 15:06:24 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Рақами воҳиди дарозиро ворид кунед (1 — дм, 2 — км, 3 — м, 4 — мм, 5 — см):");
+int vohid = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Дарозии сегментро дар воҳидҳои муқарраршуда ворид кунед:");
+double darozi = double.Parse(Console.ReadLine());
+
+double lengthInMeters = 0;
+
+switch (vohid)
+{
+    case 1: 
+        lengthInMeters = darozi * 0.1;
+        break;
+    case 2: 
+        lengthInMeters = darozi * 1000;
+        break;
+    case 3: 
+        lengthInMeters = darozi;
+        break;
+    case 4: 
+        lengthInMeters = darozi * 0.001;
+        break;
+    case 5: 
+        lengthInMeters = darozi * 0.01;
+        break;
+    default:
+        Console.WriteLine("Хатогӣ: Рақами воҳиди дарозӣ бояд аз 1 то 5 бошад.");
+        return;
+}
+
+Console.WriteLine($"Длина отрезка в метрах: {lengthInMeters}");
---
+++ Program2.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Рақами воҳиди массаро ворид кунед (1 — кг, 2 — мг, 3 — г, 4 — т, 5 — в):");
+int vohid = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Вазни бадани худро дар воҳидҳои зерин ворид кунед:");
+double massa = double.Parse(Console.ReadLine());
+
+double massakilogram = 0;
+
+switch (vohid)
+{
+    case 1: 
+        massakilogram = massa;
+        break;
+    case 2: 
+        massakilogram = massa * 0.000001;
+        break;
+    case 3: 
+        massakilogram = massa * 0.001;
+        break;
+    case 4: 
+        massakilogram = massa * 1000;
+        break;
+    case 5: 
+        massakilogram = massa * 100;
+        break;
+    default:
+        Console.WriteLine("Ошибка: номер единицы массы должен быть от 1 до 5.");
+        return;
+}
+
+Console.WriteLine(massakilogram);
